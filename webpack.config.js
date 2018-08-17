e4a13ff3f2cb9db617fcdf9edd8879c750963e87
AzureAD\JoshuaKlimaszewski <joshua@autobooks.co>
2018-08-17 16:58:49 -0400

    added polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const config = {
-    entry: './lib/components/Index.js',
+    entry: ['babel-polyfill', './lib/components/Index.js'],
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
