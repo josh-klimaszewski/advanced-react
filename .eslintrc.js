cadaf8fd70b8a2bc812daddccac602a1e9fb68b4
AzureAD\JoshuaKlimaszewski <joshua@autobooks.co>
2018-08-17 16:51:03 -0400

    webpack + react boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,11 @@ module.exports = {
     },
     "plugins": [ "react" ],
     "rules": {
-      "react/prop-types": ["off"],
-      "indent": ["error", 2],
-      // "linebreak-style": ["error","unix"],
-      "quotes": ["error","single"],
-      "semi": ["error","always"],
-      "no-console": ["warn", { "allow": ["info", "error"] }]
+    //   "react/prop-types": ["off"],
+    //   "indent": ["error", 2],
+    //   "linebreak-style": ["error","unix"],
+    //   "quotes": ["error","single"],
+    //   "semi": ["error","always"],
+    //   "no-console": ["warn", { "allow": ["info", "error"] }]
     }
   };
